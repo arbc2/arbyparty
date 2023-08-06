387fbc8085dac9ccbaa1d7a78eb49b673765dda3
arb dog <101073037+arbc2@users.noreply.github.com>
2023-08-06 13:37:26 -0700

    Add files via upload

---
+++ load.js
@@ -0,0 +1,6 @@
+$(function(){
+      $("#greenheader").load("https://arby.party/assets/important/greenheader.html"); 
+      $("#footer").load("https://arby.party/assets/important/footer.html");
+       $("#sidebar").load("https://arby.party/assets/important/sidebar.html");
+        $("#bloglinks").load("https://arby.party/assets/important/bloglinks.html");
+});
